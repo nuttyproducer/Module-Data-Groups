36579c9595b1f47fdf3a2a46954f9d99ab7c8b89
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-29 00:50:37 +0100

    Removed Prep folder from coursework/sprint-1

--- Prep/summation.test.js
+++
@@ -1,9 +0,0 @@
-const sumValues = require("./summation");
-
-test("calculates the mean of a list of numbers", () => {
-  const list = [3, 50, 7];
-  const currentOutput = sumValues(list);
-  const targetOutput = 60;
-
-  expect(currentOutput).toEqual(targetOutput);
-});
