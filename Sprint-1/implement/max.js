d8e7ba3f18af230a768d4a8fe3fc6e82ecd8cb9e
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-05 15:58:29 +0200

    Implement max and add tests

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,15 @@
-function findMax(elements) {
+function findMax(arrayElements) {
+  let max = -Infinity; // define max first, if the array is empty, return -Infinity
+  for (i = 0; i < arrayElements.length; i++) {
+    if (typeof arrayElements[i] === "number") {
+      // first checks if the array element is a number before moving on.
+      if (arrayElements[i] > max) {
+        // Finds the largest number after the === numbers test.
+        max = arrayElements[i]; // redefine max with the maximum number in the array.
+      }
+    }
+  }
+  return max;
 }
 
 module.exports = findMax;
--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -1,9 +1,9 @@
 /* Find the maximum element of an array of numbers
 
-In this kata, you will need to implement a function that find the largest numerical element of an array.
+In this kata, you will need to implement a function that finds the largest numerical element of an array.
 
 E.g. max([30, 50, 10, 40]), target output: 50
-E.g. max(['hey', 10, 'hi', 60, 10]), target output: 60 (sum ignores any non-numerical elements)
+E.g. max(['hey', 10, 'hi', 60, 10]), target output: 60 (max ignores any non-numerical elements)
 
 You should implement this function in max.js, and add tests for it in this file.
 
@@ -16,28 +16,59 @@ const findMax = require("./max.js");
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-test.todo("given an empty array, returns -Infinity");
+
+test("given an empty array, returns -Infinity", () => {
+  expect(findMax([])).toBe(-Infinity);
+});
 
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
 
+test("given an array with one number, returns that number", () => {
+  expect(findMax([44])).toBe(44);
+});
+
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
+
+test("given an array with both positive and negative numbers, returns the largest number overall", () => {
+  expect(findMax([-10, 0, 5, 30, -40])).toBe(30);
+});
 
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
 
+test("given an array with only negative numbers, returns the number closest to zero", () => {
+  expect(findMax([-20, -4, -43, -33, -7])).toBe(-4);
+});
+
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
+
+test("given an array with decimal numbers, returns the largest decimal number", () => {
+  expect(findMax([2.3, 4.4, 5.5, 1.2, 6.6])).toBe(6.6);
+});
 
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
 
+test("given an array with non-number values, returns the max number and ignores non-numeric values", () => {
+  expect(findMax(["Spongebob", 2, "Squarepants", 5, 8, "patrick", 20])).toBe(
+    20
+  );
+});
+
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
+
+test("given an array with only non-numeric values, returns -Infinity", () => {
+  expect(findMax(["Spongebob", "Squarepants", null, undefined])).toBe(
+    -Infinity
+  );
+});
