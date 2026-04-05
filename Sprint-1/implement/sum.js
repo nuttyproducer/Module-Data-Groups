883afa58a4474945b039f7d1cd897d739a596c9e
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-05 15:58:18 +0200

    Implement sum and add tests

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,13 @@
 function sum(elements) {
+  let total = 0;
+
+  for (let i = 0; i < elements.length; i++) {
+    if (typeof elements[i] === "number") {
+      total = total + elements[i];
+    }
+  }
+
+  return total;
 }
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,47 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+
+test("given an empty array, returns 0", () => {
+  expect(sum([])).toBe(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
 
+test("given an array with one number, returns that number", () => {
+  expect(sum([44])).toBe(44);
+});
+
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+
+test("given an array containing negative numbers, returns the correct total sum", () => {
+  expect(sum([-10, 5, -3, 8])).toBe(0);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
 
+test("given an array with decimal numbers, returns the correct total sum", () => {
+  expect(sum([1.5, 2.5, 3.5])).toBe(7.5);
+});
+
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
 
+test("given an array containing non-number values, ignores them and returns the sum of numbers", () => {
+  expect(sum(["hey", 10, "hi", 60, 10])).toBe(80);
+});
+
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+
+test("given an array with only non-number values, returns 0", () => {
+  expect(sum(["hello", null, undefined, "banana"])).toBe(0);
+});
