1a8b7cfd70b411e2b3863be7295ed9f5f2f4e9c1
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-05 15:58:38 +0200

    Implement dedupe and add tests

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,13 @@
-function dedupe() {}
+function dedupe(list) {
+  let result = [];
+
+  for (let i = 0; i < list.length; i++) {
+    if (!result.includes(list[i])) {
+      result.push(list[i]);
+    }
+  }
+
+  return result;
+}
+
+module.exports = dedupe;
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,12 +16,32 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+test("given an empty array, it returns an empty array", () => {
+  expect(dedupe([])).toEqual([]);
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
+test("given an array with no duplicates, it returns a copy of the original array", () => {
+  expect(dedupe([1, 2, 3])).toEqual([1, 2, 3]);
+});
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+
+// Duplicated strings.
+test("given an array of duplicate strings, it removes duplicates and keeps the first occurrence", () => {
+  expect(dedupe(["a", "a", "a", "b", "b", "c"])).toEqual(["a", "b", "c"]);
+});
+
+// Duplicated numbers.
+test("given an array of duplicate numbers, it removes duplicates and keeps the first occurrence", () => {
+  expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toEqual([5, 1, 2, 3, 8]);
+});
+
+// Mixed vanlues of numbers and strings.
+test("given a mixed array with duplicates, it preserves the first occurrence of each element", () => {
+  expect(dedupe([1, "a", 1, "a", 2, "b", 2])).toEqual([1, "a", 2, "b"]);
+});
