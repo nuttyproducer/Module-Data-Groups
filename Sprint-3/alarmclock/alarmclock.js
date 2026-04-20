4109c2c00e31a126a7ecfea77bef022b0b0068a3
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-20 13:06:41 +0200

    feat: display timer in mm:ss format and initialize to 00:00

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,15 @@
-function setAlarm() {}
+function setAlarm() {
+  document.getElementById("timeValue").textContent = "00:00"; // This always makes sure the timer on the page is set to 00.00 when we reaload the page.
+  const seconds = document.getElementById("alarmSet").value; // We take the value input in seconds we have on the alarmSet id of the HTML page.
+  // Next part: Make sure we can convert our seconds into minutes and seconds and display them correctly inside the HTML file.
+  const totalSeconds = Number(seconds);
+  const mins = Math.floor(totalSeconds / 60);
+  const sec = totalSeconds % 60;
+  const timerDisplay = `${mins.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
+
+  document.getElementById("timeValue").textContent = timerDisplay;
+}
+
 
 // DO NOT EDIT BELOW HERE
 
