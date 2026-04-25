f2c12533b3e3716f1d54945e9d95ba03e7d0951c
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-25 17:19:47 +0200

    feat: render reading list with title, author, image, and background color based on read status

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,43 @@ const books = [
   },
 ];
 
+// Basic strategy
+
+/* 
+- Prepare your data: You’ll have an array of book objects, each with a title, author, alreadyRead, and bookCoverImage.
+
+- Select where to render: Find the right place in your HTML (like a div or ul) to display the list.
+
+- Loop through the books: For each book, create the necessary elements (title, author, image).
+
+- Set styles based on alreadyRead: Change the background color depending on whether the book is read.
+
+- Add everything to the page: Append your new elements to the DOM so they show up.
+*/
+document.title = "Reading list app";
+
+const bookList = document.getElementById("readingList");
+
+books.forEach((book) => {
+  const list = document.createElement("li");
+
+  if (book.alreadyRead) {
+    list.classList.add("bookNotread");
+  } else {
+    list.classList.add("BookRead");
+  }
+
+  const bookTitle = document.createElement("h3");
+  const author = document.createElement("p");
+  const bookImage = document.createElement("img");
+
+  bookTitle.textContent = book.title;
+  author.textContent = book.author;
+  bookImage.src = book.bookCoverImage;
+
+  list.appendChild(bookTitle);
+  list.appendChild(author);
+  list.appendChild(bookImage);
+
+  bookList.appendChild(list);
+});
