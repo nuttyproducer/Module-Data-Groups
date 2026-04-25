51992bed26e4c5f26597e11045c7d33f3d47b8a5
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-25 21:56:36 +0200

    Add deleteCompleted todo logic

--- Sprint-3/todo-list/todos.mjs
+++ Sprint-3/todo-list/todos.mjs
@@ -27,3 +27,11 @@ export function toggleCompletedOnTask(todos, taskIndex) {
     todos[taskIndex].completed = !todos[taskIndex].completed;
   }
 }
+
+export function deleteCompleted(todos) {
+  for (let i = todos.length - 1; i >= 0; i--) {
+    if (todos[i].completed) {
+      todos.splice(i, 1);
+    }
+  }
+}
