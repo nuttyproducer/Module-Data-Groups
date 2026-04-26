911638a52955ec870ed1b6236399e1d6a7e900c2
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-04-26 23:14:12 +0200

    Add button functionality to forward and backward buttons

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,35 @@
+// Array of images
+
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-d.jpg",
 ];
 
-
 // Write your code here
+const imageElement = document.getElementById("carousel-img");
+const forwardBtn = document.getElementById("forward-btn");
+const backwardBtn = document.getElementById("backward-btn");
+
+let currentImage = 0;
+imageElement.src = images[currentImage];
+
+function showNextImage() {
+  currentImage = currentImage + 1;
+  if (currentImage >= images.length) {
+    currentImage = 0;
+  }
+  imageElement.src = images[currentImage];
+}
+
+function showPreviousImage() {
+  currentImage = currentImage - 1;
+  if (currentImage < 0) {
+    currentImage = images.length - 1;
+  }
+  imageElement.src = images[currentImage];
+}
+
+forwardBtn.addEventListener("click", showNextImage);
+backwardBtn.addEventListener("click", showPreviousImage);
